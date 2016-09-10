70760b1d5ba26a8e3463f968503bfdfbcd2ff3a2
Kenneth Raven Miranda <krpmiranda@yahoo.com>
2016-09-10 11:40:12 +0800

    adds strcat

--- lab3.cpp
+++ lab3.cpp
@@ -22,11 +22,16 @@ void myStrCpy(char* str1, char* str2){
 	strcpy(str1, str2);
 }
 
+void myStrCat(char* str1, char* str2){
+	strcat(str1, str2);
+}
+
 int main(){
 	char str1[100]="Hello";
 	char str2[100]="hello";
 	
 	myStrCmp(str1, str2);
 	myStrCpy(str1, str2);
+	myStrCat(str1, str2);
 	cout << str1;
 }
