7fc40ac5db1c04c996f7070ff029a4b95ada9d51
Kenneth Miranda <krpmiranda@yahoo.com>
2016-09-17 12:14:04 +0800

    Added Lab 3 Problem #2

--- lab5.cpp
+++ lab5.cpp
@@ -53,3 +53,61 @@ void newLine(){
     char s;
     do{cin.get(s);}while(s!='\n');     
 }
+
+/****
+LAB 3 PROBLEM #2
+#include <iostream>
+using namespace std;
+
+struct Name{
+   char givenName[30];
+};
+
+struct Student{
+   double id[5];
+   Name name[5];
+   double grade[3];
+   float ave[3], sum[3];  
+};
+
+void newLine();
+
+int main(){
+    Student stud[5];  
+    for (int i=0; i<5; i++){
+    	cout << "Student: " << i+1 << endl;
+    	cout << "ID: ";
+    	cin >> stud[i].id[i];
+    	newLine();
+    	cout << "Name: ";
+		cin.getline(stud[i].name[i].givenName, 29);
+		cout << "Enter 3 quizzes: ";
+		for(int j=0; j<3; j++){
+	        cin >> stud[i].grade[j];
+    	}
+	}
+	
+	cout << "No.\t\tStudent No.\t\t\tName\t\t\tGrade\t\tRemark\n";
+    for (int i=0; i<5; i++){
+    	cout << i+1 << "\t\t" << stud[i].id[i] << "\t\t\t\t" << stud[i].name[i].givenName << "\t\t\t";
+    	for(int j=0; j<3; j++){
+        	stud[i].sum[i] += stud[i].grade[j];  
+    	}
+    	
+	    stud[i].ave[i] = stud[i].sum[i]/3;
+	    cout << stud[i].ave[i];
+	    
+	    if(stud[i].ave[i]>=75)
+	    	cout << "\t\tPassed\n";
+		else
+			cout << "\t\tFailed\n";
+	}
+    
+    system("pause > 0");
+    return 0;   
+}
+void newLine(){
+    char s;
+    do{cin.get(s);}while(s!='\n');     
+}
+****/
